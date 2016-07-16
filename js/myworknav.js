7e8c93a7001e654ac8ddc6db5d151ed701abe9da
Steven <info@truediggs.com>
2016-07-15 23:00:52 -0400

    newdesign

---
+++ js/myworknav.js
@@ -0,0 +1,39 @@
+
+$("#td").click(function(){
+    	$(".tdlanding").show( function (){
+    		$(".myworklanding").hide(function () {
+    		$(".tdlanding").get(0).scrollIntoView(function() {
+
+$("#back").click(function(){
+	$(".tdlanding").hide(function () {
+
+		  });
+    	
+  });
+    });
+    });
+    });
+});
+
+
+$("#yutli").click(function(){
+    	$(".yutlilanding").show( function (){
+    		$(".myworklanding").hide(function () {
+    		$(".yutlilanding").get(0).scrollIntoView(function() {
+    			
+
+ $("#back").click(function(){
+	$(".yutlilanding").hide(function () {   	
+
+  
+
+
+
+    });
+    	
+  });
+    });
+    });
+    });
+});
+
