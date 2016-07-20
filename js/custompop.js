44b157fe46bef4b6b0f0ebce31fb0a26970fddad
Steven <info@truediggs.com>
2016-07-20 11:36:26 -0400

    changes to mywork

---
+++ js/custompop.js
@@ -0,0 +1,15 @@
+    $(document).ready(function() {
+
+
+
+$('#yutlione').popup(function() {
+  scrolllock: true 
+
+$('#yutlione').popup({
+ onopen: function() {
+  (".myworklanding").hide();
+
+}
+});
+});
+});
--- js/myworknav.js
+++ js/myworknav.js
@@ -1,39 +1,40 @@
-
-$("#td").click(function(){
-    	$(".tdlanding").show( function (){
-    		$(".myworklanding").hide(function () {
-    		$(".tdlanding").get(0).scrollIntoView(function() {
-
-$("#back").click(function(){
-	$(".tdlanding").hide(function () {
-
-		  });
-    	
-  });
-    });
-    });
-    });
-});
-
-
-$("#yutli").click(function(){
-    	$(".yutlilanding").show( function (){
-    		$(".myworklanding").hide(function () {
-    		$(".yutlilanding").get(0).scrollIntoView(function() {
-    			
-
- $("#back").click(function(){
-	$(".yutlilanding").hide(function () {   	
-
-  
-
+function yutliAnim() { 
+    $( ".yutlilanding").slideDown(function() {
+    $( ".myworklanding").fadeOut(function () {
 
 
     });
-    	
-  });
+   });  
+    
+      
+        }
+
+function yuliClose() {
+    $( ".myworklanding").slideDown("fast", function() {
+    $(".yutlilanding").hide(function () {
+
     });
+   }); 
+}
+
+
+function quoteAnim() { 
+    $( ".quotelanding").slideDown(function() {
+    $( ".myworklanding").fadeOut(function () {
+
+
     });
+   });  
+    
+      
+        }
+
+function quoteClose() {
+    $( ".myworklanding").slideDown("fast", function() {
+    $(".quotelanding").hide(function () {
+
     });
-});
+   }); 
+}
+
 
